b86cfdda1d1ba3f292879dddd588046fc32605a7
ds99086 <ds99086@gmail.com>
2021-06-29 22:25:06 +1200

    trailwind based layout

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'body': ['Ubuntu'],
+        'heading':['Alegreya'],
+        'prompt': ['Prompt']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
